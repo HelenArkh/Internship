bbc9ab67e8f75738e38b2f3a2883c3db03587820
HelenArkh <alena.arkh@gmail.com>
2021-11-27 18:15:41 +0300

    Step 2

--- Internship/Internship/RegisterController.cs
+++ Internship/Internship/RegisterController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Internship.ViewModels.Account;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,31 @@ namespace Internship
         [HttpGet]
         public IActionResult RegisterPart2(RegisterViewModel model)
         {
+            return View("RegisterPart2", model);
+        }
+
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            //if (ModelState.IsValid)
+            //{
+            //    var user = _mapper.Map<User>(model);
+
+            //    var result = await _userManager.CreateAsync(user, model.PasswordReg);
+            //    if (result.Succeeded)
+            //    {
+            //        await _signInManager.SignInAsync(user, false);
+            //        return RedirectToAction("Index", "Home");
+            //    }
+            //    else
+            //    {
+            //        foreach (var error in result.Errors)
+            //        {
+            //            ModelState.AddModelError(string.Empty, error.Description);
+            //        }
+            //    }
+            //}
             return View("RegisterPart2", model);
         }
     }
